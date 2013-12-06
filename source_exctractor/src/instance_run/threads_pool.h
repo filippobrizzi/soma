de2c75af9eb2263123f89d6fdf793dbf37b9e3c0
pippo <giacomo.dabisias@gmail.com>
2013-12-06 12:44:38 +0100

    ThreadPool almost complete, just to fix costructor

--- source_exctractor/src/instance_run/threads_pool.h
+++ source_exctractor/src/instance_run/threads_pool.h
@@ -1,3 +1,38 @@
+#include <string>
+#include <thread>
+#include <vector>
+#include <mutex> 
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <condition_variable>
+
+#include "xml_creator/tinyxml2.h"
+
+int chartoint(const char *cc);
+int chartoint(char *cc);
+
+
+class ForParameter {
+public:
+    int thread_id_;
+    int num_threads_;
+    ForParameter(int thread_id, int num_threads) : thread_id_(thread_id), num_threads_(num_threads) {}
+};
+
+
+class NestedBase { 
+public: 
+
+    NestedBase(int pragma_id) : pragma_id_(pragma_id) {}
+
+    int pragma_id_;
+    //int activation_time_;
+    
+    virtual void callme(ForParameter for_param) = 0;
+    
+    void operator()(ForParameter for_param) { callme(for_param); }
+};
 
 
 class ThreadPool {
@@ -15,13 +50,16 @@ public:
 
     void init(int pool_size);
 
-    jobid_t push(Job job);
+    void call(std::shared_ptr<NestedBase> nested_base);
+
+    void push(std::shared_ptr<NestedBase> nested_base, ForParameter for_param);
+    void push_termination_job();
 
     bool join(jobid_t);
 
     void joinall();
 
-    void call(std::shared_ptr<NestedBase> nested_base);
+    static ThreadPool* getInstance(std::string file_name);
 
     ~ThreadPool() { joinall(); }
 
@@ -29,6 +67,8 @@ public:
 private:
     struct ScheduleOptions {
         int pragma_id_;
+        /* Idicates the pragma type: parallel, task, ... */
+        std::string pragma_type_;
         /* Indicates the threads that have to run the task */
         std::vector<int> threads_
         /* List of pragma_id_ to wait before completing the task */
@@ -38,82 +78,25 @@ private:
     struct JobIn {
         Job job_;
         jobid_t job_id_;
+        std::string job_type_;
+        /* Communicates to threads to terminate their activity */
+        bool exit_run_ = false;
         bool terminated_with_exceptions_;
         std::unique_ptr<std::condition_variable> done_cond_var_;
     };
 
+    ThreadPool(std::string file_name);
+    
     void run(int id);
 
-    std::map<int, std::Queue<JobIn>> work_queue_; //NB thread safe
-    
     std::map<int, ScheduleOptions> sched_opt_;
-    
+
     std::vector<std::thread> threads_pool_; // not thread safe
-    std::map<jobid_t, JobIn> known_jobs_; // thread safe
-    
-    std::mutex mtx; // common
+    /* Job queue */
+    std::Queue<JobIn> work_queue_;    
+    /* For each pragma the list of jobs executing that pragma, e.g. in case of parallel for */
+    std::map<jobid_t, std::vector<JobIn>> known_jobs_;
+    /* Mutex used by std::condition_variable to synchronize jobs execution */
+    std::mutex cond_var_mtx; 
 };
 
-void ThreadPool::push(std::shared_ptr<NestedBase> nested_base, ForParameter for_param, int thread_id) {
-    jobid_t id = nested_base.pragma_id_;
-        
-    JobIn job_in;
-
-    job_in.job_ = Job(nested_base, for_param);
-    job_in.job_id_ = id;
-    job_in.done_cond_var_ = std::unique<std::condition_variable>(new std::condition_variable());
-    known_jobs_[id] = job_in;
-    work_queue_[id].push_back(job_in_);
-    
-    return id;
-}
-
-void ThreadPool::call(std::shared_ptr<NestedBase> nested_base) {
-    int thread_number = sched_opt_[nested_base->pragma_id_].threads_.size();
-    int thread_id;
-    for(int i = 0; i < thread_number; i ++) {
-        thread_id = sched_opt_[nested_base->pragma_id_].threads_.at(i);
-        push(nested_base, ForParameter(i, thread_number), thread_id);
-    }
-
-    int barriers_number = sched_opt_[nested_b->pragma_id_]).barriers_.size();
-    int barriers_id = sched_opt_[nested_b->pragma_id_].barriers_.at(i);
-    for (int i = 0; i < barriers_number; i ++) {
-        join(barriers_id);
-        known_jobs_.erase(barriers_id);
-    }
-
-}
-
-
-void ThreadPool::init(int pool_size)
-{
-    threads_pool_.reserve(pool_size);
-    for(int i = 0; i < pool_size; i++) {
-        threads_pool_.push_back(std::thread(&ThreadPool::run,this, i));
-    }
-}
-
-void ThreadPool::run(int me) {
-        while(true) {
-            JobIn job_in = work_queue_[me].front();
-                    // TODO: exceptions here: terminatedwithexceptions
-            try {
-                job_in.job_.nested_base_(job_in.job_.for_param_);
-            } catch(std::exception e)
-            
-            job_in.done_cond_var_.notify_one();
-        }
-}
-
-void ThreadPool::join(jobid_t job_id) {
-    
-    std::unique_lock<std::mutex> lk(mtx);
-    known_jobs_[job_id].done_cond_var_.wait(lk);
-}
-
-void ThreadPool::joinall() {
-        work_queue_.clear();
-        for(int i = 0; i < threads_pool_.size(); i++)
-                threads_pool_[i].join();
-}
